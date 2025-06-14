b461a4bba000c55beb45ac981848522ac73466a0
mantlar <irolph@hotmail.com>
2025-06-14 02:16:27 -0700

    removed batch normalization layers temporarily

--- dqn.py
+++ dqn.py
@@ -18,14 +18,12 @@ class DQN(nn.Module):
     def __init__(self, state_size, action_size):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(state_size, 128)
-        self.bn1 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 128)
-        self.bn2 = nn.BatchNorm1d(128)
         self.fc3 = nn.Linear(128, action_size)
-        
+    
     def forward(self, x):
-        x = torch.relu(self.bn1(self.fc1(x)))
-        x = torch.relu(self.bn2(self.fc2(x)))
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
         return self.fc3(x)
 
 class DQNAgent:
